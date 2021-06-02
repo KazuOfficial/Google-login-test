d5235c4336f386992876b268d8cc90ce89179176
KazuOfficial <kazuofficial.contact@gmail.com>
2021-06-02 14:25:00 +0200

    Finished API

---
+++ GoogleAuthenticationExample/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Google_Authentication_Example.Controllers
+{
+    [AllowAnonymous, Route("auth")]
+    public class AccountController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleResponse") };
+
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [Authorize]
+        [Route("response")]
+        public async Task<IActionResult> Response()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities.FirstOrDefault()
+                .Claims.Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+            return Json(claims);
+        }
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Google login test
-
-[![Build 
-Status](https://travis-ci.com/KazuOfficial/Google-login-test.svg?branch=master)](https://travis-ci.com/KazuOfficial/Google-login-test)
